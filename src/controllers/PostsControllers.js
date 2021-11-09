eb5e3a11ff33b22ab17e1cab565d58f2d2bdbe86
Jean Fraga <jeanfragoli395@gmail.com>
2021-11-09 12:19:18 -0300

    funcao para listar os posts criada

--- src/controllers/PostsControllers.js
+++ src/controllers/PostsControllers.js
@@ -7,8 +7,17 @@ class PostsControllers {
     try{
        const newPost = await db.posts.create({titulo, conteudo});
        return res.status(201).json(newPost);
-       
+
     } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
+
+  static async list (req, res) {
+    try {
+      const allPosts = await db.posts.findAll();
+      return res.status(200).json(allPosts);
+    }catch (err) {
       return res.status(500).json(err);
     }
   }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const router = Router();
 const PostsControllers = require('../controllers/PostsControllers')
 
-router.post('/posts', PostsControllers.add)
+router.post('/posts', PostsControllers.add);
+router.get('/posts', PostsControllers.list);
 
 module.exports =  router
