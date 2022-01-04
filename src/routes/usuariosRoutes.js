cc9da3f18388b8e3071e33c33ff2e8a3b4cae2f1
Jean Fraga <jeanfragoli395@gmail.com>
2022-01-04 10:13:39 -0300

    Rota para redefinição de senha  criada

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -6,12 +6,13 @@ const middlewareDeAuthorizacao = require('../middlewares/middleware-de-autorizac
 const router = Router()
 
 router.post('/usuarios', UsuariosControllers.add);
-router.post('/usuarios/login', middlewareDeAuthenticacao.local, UsuariosControllers.login)
+router.post('/usuarios/esqueci-minha-senha');
+router.post('/usuarios/login', middlewareDeAuthenticacao.local, UsuariosControllers.login);
 router.get('/usuarios', [middlewareDeAuthenticacao.bearer, middlewareDeAuthorizacao('usuarios', 'ler')], UsuariosControllers.list);
 router.delete('/usuarios/:userID',[middlewareDeAuthenticacao.bearer, middlewareDeAuthenticacao.local, middlewareDeAuthorizacao('usuarios', 'remover')],  UsuariosControllers.remove);
-router.post('/usuarios/logout',[ middlewareDeAuthenticacao.refresh, middlewareDeAuthenticacao.bearer ], UsuariosControllers.logout)
+router.post('/usuarios/logout',[ middlewareDeAuthenticacao.refresh, middlewareDeAuthenticacao.bearer ], UsuariosControllers.logout);
 router.post('/usuarios/update_token', middlewareDeAuthenticacao.refresh, UsuariosControllers.login);
-router.get('/usuarios/verifica_email/:token', middlewareDeAuthenticacao.verificacaoEmail, UsuariosControllers.verificaEmail)
+router.get('/usuarios/verifica_email/:token', middlewareDeAuthenticacao.verificacaoEmail, UsuariosControllers.verificaEmail);
 
 
 module.exports = router
